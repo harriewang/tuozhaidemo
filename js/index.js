4ed93f72419763c59ac8bcfa1e308c4b98ca87d5
Harrie <harriewang@hotmail.com>
2011-04-09 00:16:28 +0800

    splashpage

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
 Ext.setup({
     tabletStartupScreen: 'tablet_startup.png',
     phoneStartupScreen: 'phone_startup.png',
-    icon: 'img/icon.png',
-    glossOnIcon: false,
+    icon: '../img/icon.png',
+	indicator:true,
+    glossOnIcon: true,
     onReady: function() {
         // Create a Carousel of Items
         var carousel1 = new Ext.Carousel({
             defaults: {
                 cls: 'card'
             },
-            ui: 'dark',
+			height:100,
+            ui: 'light',
             items: [{
-                html: '<div id="top"><ul><li><img src="img/avatar1.jpeg" width="50" height="50"></li><li><img src="img/avatar2.jpeg" width="50" height="50"></li><li><img src="img/avatar3.jpeg" width="50" height="50"></li><li><img src="img/avatar4.jpeg" width="50" height="50"></li><li><img src="img/avatar5.jpeg" width="50" height="50"></li></ul></div><div id="main" style="text-align:center;"><div id="maincontent"><img src="img/homemain.png" style="width:100%"></div></div>'
+                html: '<div id="top"><ul><li><img src="img/avatar1.jpeg" width="50" height="50"></li><li><img src="img/avatar2.jpeg" width="50" height="50"></li><li><img src="img/avatar4.jpeg" width="50" height="50"></li><li><img src="img/avatar3.jpeg" width="50" height="50"></li><li><img src="img/avatar5.jpeg" width="50" height="50"></li></ul></div><div id="main" style="text-align:center;"><div id="maincontent"><img src="img/homemain.png" style="width:100%"></div></div>'
             },
             {
                 title: 'Tab 2',
-                html: '<div id="top"><ul><li><img src="img/avatar1.jpeg" width="50" height="50"></li><li><img src="img/avatar2.jpeg" width="50" height="50"></li><li><img src="img/avatar3.jpeg" width="50" height="50"></li><li><img src="img/avatar4.jpeg" width="50" height="50"></li><li><img src="img/avatar5.jpeg" width="50" height="50"></li></ul></div><div id="main" style="text-align:center;"><div id="maincontent"><img src="img/homemain.png" style="width:100%"></div></div>'
+                html: '<div id="top"><ul><li><img src="img/avatar2.jpeg" width="50" height="50"></li><li><img src="img/avatar1.jpeg" width="50" height="50"></li><li><img src="img/avatar3.jpeg" width="50" height="50"></li></ul></div><div id="main" style="text-align:center;"><div id="maincontent"><img src="img/homemain.png" style="width:100%"></div></div>'
             },
             {
                 title: 'Tab 3',
-                html: '<div id="top"><ul><li><img src="img/avatar1.jpeg" width="50" height="50"></li><li><img src="img/avatar2.jpeg" width="50" height="50"></li><li><img src="img/avatar3.jpeg" width="50" height="50"></li><li><img src="img/avatar4.jpeg" width="50" height="50"></li><li><img src="img/avatar5.jpeg" width="50" height="50"></li></ul></div><div id="main" style="text-align:center;"><div id="maincontent"><img src="img/homemain.png" style="width:100%"></div></div>'
+                html: '<div id="top"><ul><li><img src="img/avatar4.jpeg" width="50" height="50"></li><li><img src="img/avatar5.jpeg" width="50" height="50"></li></ul></div><div id="main" style="text-align:center;"><div id="maincontent"><img src="img/homemain.png" style="width:100%"></div></div>'
             }]
         });
         
@@ -27,7 +29,7 @@ Ext.setup({
         new Ext.Panel({
             fullscreen: true,
             layout: {
-                type: 'vbox',
+                type: 'card',
                 align: 'stretch'
             },
             defaults: {
